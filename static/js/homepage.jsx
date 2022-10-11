53403ab357dc2a82149e853dd50601b1ede7081f
Carina Ogbat <carinawrightogbat@gmail.com>
2022-10-11 16:16:21 -0700

    added homepage as fragments

--- static/js/homepage.jsx
+++ static/js/homepage.jsx
@@ -1,7 +1,11 @@
 'use strict';
 
 function Homepage() {
-  return <div>This component needs to be implemented.</div>;
+  return (<React.Fragment>
+    <p>This is a website where you can view your trading cards!</p>
+    <a href="/cards">You can view your cards here.</a>
+    <img src="/static/img/balloonicorn.jpg"></img>
+    </React.Fragment>);
 }
 
 ReactDOM.render(<Homepage />, document.querySelector('#app'));
